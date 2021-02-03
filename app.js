5a7256e943c0b715a3c0289e7886f6763231d594
Ciremy <aymeric.lavaud@gmail.com>
2021-02-03 20:10:12 +0100

    add turn mecanic

--- app.js
+++ app.js
@@ -68,24 +68,118 @@ const attacks = [
         imgUrl : "./assets/img/tempete.png",
         functionClick :"guerisonPlante()"
     },
+    {
+        name : "Charge",
+        damage : 16,
+        manaCost : 4,
+        attackType : "Physique",
+        imgUrl : "./assets/img/charge.jpg",
+        functionClick :"charge()"
+    },
+    {
+        name : "Morsure venimeuse",
+        damage : 22,
+        manaCost : 7,
+        attackType : "Maladie",
+        imgUrl : "./assets/img/morsure.png",
+        functionClick :"morsureVenimeuse()"
+    },
+    {
+        name : "Piétinement",
+        damage : 8,
+        manaCost : 2,
+        attackType : "Physique",
+        imgUrl : "./assets/img/pietinement.svg",
+        functionClick :"pietinement()"
+    },
+    {
+        name : "Frappe sacrée",
+        damage : 7,
+        manaCost : 2,
+        attackType : "Lumière",
+        imgUrl : "./assets/img/frappe_sacree.jpg",
+        functionClick :"frappeSacree()"
+    },
+    {
+        name : "Zone sacrée",
+        damage : 15,
+        manaCost : 4,
+        attackType : "Lumière",
+        imgUrl : "./assets/img/zone_sacree.jpg",
+        functionClick :"zoneSacree()"
+    },
+    {
+        name : "Ultime recours",
+        damage : 40,
+        manaCost : 20,
+        attackType : "Lumière",
+        imgUrl : "./assets/img/ultime_recours.jpeg",
+        functionClick :"ultimeRecours()"
+    },
+    {
+        name : "Ombre grandissante",
+        damage : 32,
+        manaCost : 18,
+        attackType : "Ombre",
+        imgUrl : "./assets/img/ombre_grandissante.jpg",
+        functionClick :"ombreGrandissante()"
+    },
+    {
+        name : "Vision horrifique",
+        damage : 26,
+        manaCost : 11,
+        attackType : "Ombre",
+        imgUrl : "./assets/img/vision_horrifique.jpg",
+        functionClick :"visionHorrifique()"
+    },
+    {
+        name : "Ombre enveloppante",
+        damage : 18,
+        manaCost : 7,
+        attackType : "Ombre",
+        imgUrl : "./assets/img/ombre_enveloppante.jpg",
+        functionClick :"ombreEnveloppante()"
+    },
+
 ]
 
 const monsters = [
     {
         name : "Zombie",
         imgUrl : "./assets/img/zombie.jpg",
-        lifePoint : 74,
-        mana : 15,
+        lifePoint : 84,
+        mana : 12,
         availableAttacks : ["Attaque de base","Peste virulente", "Lacération", "Vomi immonde" ]
 
     },
     {
         name : "Druide",
         imgUrl : "./assets/img/druide.jpeg",
-        lifePoint : 62,
-        mana : 25,
+        lifePoint : 59,
+        mana : 27,
         availableAttacks : ["Attaque de base","Attaque de ronce", "Rage animal", "Tempête meurtrières" ]
 
+    },
+    {
+        name : "Chimère",
+        imgUrl : "./assets/img/chimere.jpg",
+        lifePoint : 62,
+        mana : 19,
+        availableAttacks : ["Attaque de base","Charge", "Morsure venimeuse", "Piétinement" ]
+    },
+    {
+        name : "Paladin",
+        imgUrl : "./assets/img/paladin.png",
+        lifePoint : 95,
+        mana : 22,
+        availableAttacks : ["Attaque de base","Frappe sacrée", "Zone sacrée", "Ultime recours" ]
+    },
+    {
+        name : "Spectre ombreux",
+        imgUrl : "./assets/img/spectre_ombreux.jpg",
+        lifePoint : 51,
+        mana : 35,
+        availableAttacks : ["Attaque de base","Ombre grandissante", "Vision horrifique", "Ombre enveloppante" ]
     },
      //TODO : add other monsters
 ]
@@ -177,6 +271,32 @@ function ressources(monsterData, playerNb){
 
 // }
 
+let playerTurn ;
+
+function choseFisrtPlayer(){
+    playerTurn = Math.floor((Math.random()*2)+1);
+    console.log(playerTurn);
+    document.getElementById(`player${playerTurn}Turn`).innerHTML = "It's your turn to play";
+    if(playerTurn ==1){
+        document.getElementById(`player${playerTurn+1}Turn`).innerHTML = "It's not your turn to play";
+    }else{
+        document.getElementById(`player${playerTurn-1}Turn`).innerHTML = "It's not your turn to play";
+    }
+    
+}
+
+function switchingTurn(){
+    if(playerTurn == 1){
+        playerTurn=2;
+        document.getElementById(`player${playerTurn}Turn`).innerHTML = "It's your turn to play";
+        document.getElementById(`player${playerTurn-1}Turn`).innerHTML = "It's not your turn to play";
+    }else{
+        playerTurn=1;
+        document.getElementById(`player${playerTurn}Turn`).innerHTML = "It's your turn to play";
+        document.getElementById(`player${playerTurn+1}Turn`).innerHTML = "It's not your turn to play";
+    }
+}
+
 function winTest(){
     if(actualPlayersRessources[0].lifePoint<0){
         alert("player 2 won");
@@ -193,44 +313,52 @@ function spellUse(spellDamage,spellMana,numPlayer){
     if (numPlayer==1){
         actualPlayersRessources[1].lifePoint = actualPlayersRessources[1].lifePoint-spellDamage;
         actualPlayersRessources[0].mana =actualPlayersRessources[0].mana-spellMana;
-        if (actualPlayersRessources[0].mana>=0){
-            document.getElementById(`player2Hp`).innerHTML =actualPlayersRessources[1].lifePoint+" Hp";
-            document.getElementById(`player1Energie`).innerHTML =actualPlayersRessources[0].mana+" Mana";
+        console.log(playerTurn);
+        if(playerTurn ==1){
+            if (actualPlayersRessources[0].mana>=0){
+                document.getElementById(`player2Hp`).innerHTML =actualPlayersRessources[1].lifePoint+" Hp";
+                document.getElementById(`player1Energie`).innerHTML =actualPlayersRessources[0].mana+" Mana";
+                switchingTurn();
+            }else{
+                alert("You can't cast this spell");
+                actualPlayersRessources[0].mana =actualPlayersRessources[0].mana+spellMana;
+                actualPlayersRessources[1].lifePoint = actualPlayersRessources[1].lifePoint+spellDamage;
+            }
         }else{
-            alert("You can't cast this spell");
+            alert("It's not your turn to play");
             actualPlayersRessources[0].mana =actualPlayersRessources[0].mana+spellMana;
             actualPlayersRessources[1].lifePoint = actualPlayersRessources[1].lifePoint+spellDamage;
         }
-        //updatingRessources(actualPlayersRessources,numPlayer);
-        //updatingRessources(actualPlayersRessources[0],numPlayer);
+        
     }else{
         actualPlayersRessources[0].lifePoint = actualPlayersRessources[0].lifePoint-spellDamage;
         actualPlayersRessources[1].mana =actualPlayersRessources[1].mana-spellMana;
-        if (actualPlayersRessources[1].mana>=0){
-        document.getElementById(`player1Hp`).innerHTML =actualPlayersRessources[0].lifePoint+" Hp";
-        document.getElementById(`player2Energie`).innerHTML =actualPlayersRessources[1].mana+" Mana";
+        if(playerTurn ==2){
+            if (actualPlayersRessources[1].mana>=0){
+                document.getElementById(`player1Hp`).innerHTML =actualPlayersRessources[0].lifePoint+" Hp";
+                document.getElementById(`player2Energie`).innerHTML =actualPlayersRessources[1].mana+" Mana";
+                switchingTurn();
+            }else{
+                    alert("You can't cast this spell");
+                    actualPlayersRessources[1].mana =actualPlayersRessources[1].mana+spellMana;
+                    actualPlayersRessources[0].lifePoint = actualPlayersRessources[0].lifePoint+spellDamage;
+            }
         }else{
-            alert("You can't cast this spell");
+            alert("It's not your turn to play");
             actualPlayersRessources[1].mana =actualPlayersRessources[1].mana+spellMana;
             actualPlayersRessources[0].lifePoint = actualPlayersRessources[0].lifePoint+spellDamage;
         }
         
+        
         //updatingRessources(actualPlayersRessources,numPlayer);
     }
     winTest();
-    console.log(actualPlayersRessources);
-}
-
-let playerTurn;
-
-function choseFisrtPlayer(){
-    firstPlayerToPlay = Math.floor(Math.random()*2);
+    //console.log(actualPlayersRessources);
 }
 
 
-function switchingTurn(){
 
-}
+
 
 
 function init () {
@@ -242,7 +370,7 @@ function init () {
     //pictureAttribution(player2,numPlayer);
     ressources(player1, "1");
     ressources(player2, "2");
-        
+    choseFisrtPlayer();       
 }
 
 init()
